78c84611ced011918ec191775bc753dd8cb32fe5
dammatsu.yuta <dammatsu.yuta@moneyforward.co.jp>
2023-02-23 23:18:31 +0900

    add gorm

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,7 @@ ENV PORT 8080
 RUN apt-get update && apt-get install -y \
     git
 
-RUN go mod download
-
 WORKDIR /app
 COPY . .
+
+RUN go mod download
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
 
 func main() {
+	dsn := "root:password@tcp(db:3306)/toybox_development?charset=utf8mb4&parseTime=True&loc=Local"
+	_, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Connected to database")
+
 	r := gin.Default()
 
 	r.GET("/", func(c *gin.Context) {
